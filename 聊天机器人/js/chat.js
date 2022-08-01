35a3220fac1d330a0a0cd72f49a41da5475e5a9f
frank <172816827@qq.com>
2022-08-01 19:18:53 +0800

    Ajax第一课

---
+++ 聊天机器人/js/chat.js
@@ -0,0 +1,91 @@
+$(function(){
+    // 初始化右侧滚动条
+    // 这个方法定义在scroll.js中
+    resetui()
+
+    let send = document.querySelector('.input_sub')
+    let input_txt = document.querySelector('.input_txt')
+    let talk_list = document.querySelector('.talk_list')
+
+    send.addEventListener('click',function(){
+        let text = input_txt.value.trim()
+        // 用户输入为空 时
+        if(text.length<=0){
+            return input_txt.value = ''
+        }
+        // 输入不为空时 发送消息
+        let li = document.createElement('li')
+        li.classList.add('right_word')
+        li.innerHTML = `
+        <img src="img/person02.png" />
+        <span>${input_txt.value}</span>
+        
+        `
+        talk_list.appendChild(li)
+        input_txt.value = ''
+        resetui()
+        getMsg(text)
+    })
+
+    input_txt.addEventListener('keyup',function(e){
+        if(e.key === 'Enter'){
+            send.click()
+        }
+    })
+
+
+    function getMsg(text){
+        alert(111)
+        $.ajax({
+            method:'GET',
+            url:'http://ajax.frontend.itheima.net:3006/api/robot',
+            data:{
+                spoken : text,
+            },
+            success : function(res){
+                console.log(res);
+                if(res.message === 'success'){
+                    let msg = res.data.info.text
+                     // 输入不为空时 发送消息
+                    let li = document.createElement('li')
+                    li.classList.add('left_word')
+                    li.innerHTML = `
+                    <img src="img/person01.png" />
+                    <span>${msg}</span>
+                    
+                    `
+                    talk_list.appendChild(li)
+                    resetui()
+                    // 调用用语音函数
+                    getVoice(msg)
+
+                }else{
+                    console.log('失败');
+                }
+            }
+        })
+    }
+
+
+    function getVoice(text){
+        $.ajax({
+            method:'GET',
+            url:'http://ajax.frontend.itheima.net:3006/api/synthesize',
+            text:text,
+            success: function(res){
+                if(res.status === 2000){
+                    let audio = document.querySelector('audio')
+                    audio.style.src = res.voiceUrl
+                }
+            }
+
+
+        })
+
+
+    }
+
+
+
+
+  })
